cb98d9f8e78109b32bcdbe73a00f8abfc5d77ae1
Josh Patra <30350506+SoPat712@users.noreply.github.com>
2024-07-03 14:45:57 -0400

    name test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is gonna be my portfolio</p>
+        <p>Josh Patra's Portfolio</p>
         <a
           className="App-link"
           href="https://reactjs.org"
